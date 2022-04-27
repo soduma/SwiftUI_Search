abe12327aaecc81f7d38568de159b5d0396e39b3
soduma <soduma@naver.com>
2022-04-27 13:40:24 +0900

    Initial Commit

---
+++ SwiftUI_Search/SwiftUI_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SearchApp.swift
+//  SwiftUI_Search
+//
+//  Created by 장기화 on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
